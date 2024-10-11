7660b604048b6161c73a8d01dcba86247e7f7d02
AlinaGladkova <tina.leaf89@gmail.com>
2024-10-11 20:06:24 +0300

    The API data is taken by fetch and useEffect in GamesList. Also the basic styles for list and cards is done

--- src/components/gameList/GamesList.jsx
+++ src/components/gameList/GamesList.jsx
@@ -1,14 +1,66 @@
 import cn from "classnames";
-//sass
-import GamesCard from "../gameCard/GameCard";
+import styles from "./gamesList.module.scss";
+import GameCard from "../gameCard/GameCard";
+import { useEffect, useState } from "react";
 
 export default function GamesList() {
+  const [gameCards, setgameCards] = useState([]);
+  //https://www.freetogame.com/api
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const url = "https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc";
+  //     const options = {
+  //       method: "GET",
+  //       headers: {
+  //         "x-rapidapi-key": "dd054139admsh0ba150c40ea21dfp1f0668jsnd6c925a75ebc",
+  //         "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+  //       },
+  //     };
+
+  //     // await fetch(url, options)
+  //     //   .then((response) => response.json())
+  //     //   .then((data) => console.log(data))
+  //     //   .then((data) => setgameCards(data))
+  //     //   .catch((err) => console.log(err));
+  //     // return () => {};
+
+  //     try {
+  //       const response = await fetch(url, options);
+  //       const result = await response.json();
+
+  //       setgameCards(result);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
+  useEffect(() => {
+    const url = "https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc";
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "dd054139admsh0ba150c40ea21dfp1f0668jsnd6c925a75ebc",
+        "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => setgameCards(data))
+
+      .catch((err) => console.log(err));
+
+    return () => {};
+  }, []);
+
   return (
-    <div className="games-list">
-      <GamesCard></GamesCard>;
+    <div className={cn(styles["games-list"])}>
+      {/* {console.log(gameCards)} */}
+      {gameCards.map((gameCard) => (
+        <GameCard key={gameCard.id} data={gameCard}></GameCard>
+      ))}
     </div>
-    // <div className= cn(styles['games-list'])>
-    //   <GamesCard></GamesCard>;
-    // </div>
   );
 }
