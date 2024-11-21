790553a1747fc10d656a2a1ba29c0b6b8c2ec2ee
AlinaGladkova <tina.leaf89@gmail.com>
2024-11-21 20:05:50 +0300

    check routing

--- src/components/common/app/App.jsx
+++ src/components/common/app/App.jsx
@@ -7,7 +7,7 @@ import Layout from "../layout/Layout";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<CatalogPage />} loader={gamesCatalogLoader}></Route>
+      <Route path="/" element={<CatalogPage />} loader={gamesCatalogLoader}></Route>
       <Route path="gameProfilePage/:id" element={<GameProfilePage />} loader={gamesProfileLoader}></Route>
       <Route path="*" element={<NotFoundPage />}></Route>
     </Route>
@@ -21,19 +21,3 @@ export default function App() {
     </>
   );
 }
-
-//крашится лого в профайле ???
-
-//страница игры:
-// Если не получилось получить данные, необходимо сообщить пользователю
-
-// адаптив
-// Фреймворк UI
-// Приложение должно запускаться по адресу localhost:3001 командой npm start
-// Если карточка игры была открыта, то она должна быть доступна при последующих открытиях (перезагрузках) страницы без дополнительного запроса в течение 5 минут
-
-// Учитывать, что список игр может содержать тысячи тайтлов
-// При неудачном запросе должно быть три попытки повторного запроса
-// При переходе со страницы на страницу запросы, относящиеся к старой странице, должны прерываться (отменяться/прекращаться)
-// Бэкенд для хостинга статики и API для инкапсуляции внешних запросов на Node.JS
-// Покрытие кода юнит-тестами
