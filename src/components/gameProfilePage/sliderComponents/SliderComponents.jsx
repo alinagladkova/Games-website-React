39f89153685c0dc839620764fb8007c4fda74760
AlinaGladkova <tina.leaf89@gmail.com>
2024-12-04 23:01:30 +0300

    Updates

--- src/components/gameProfilePage/sliderComponents/SliderComponents.jsx
+++
@@ -1,9 +0,0 @@
-export function NextArrow(props) {
-  const { className, style, onClick } = props;
-  return <div className={className} style={{ ...style, display: "block", right: "5px" }} onClick={onClick} />;
-}
-
-export function PrevArrow(props) {
-  const { className, style, onClick } = props;
-  return <div className={className} style={{ ...style, display: "block", left: "5px", zIndex: "3" }} onClick={onClick} />;
-}
